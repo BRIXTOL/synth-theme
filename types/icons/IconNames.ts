b5716a6930b7732128916f2ea441fd5fddc3a0b4
Nikolas Savvidis <nicos.savidis@gmail.com>
2020-09-26 06:29:12 +0200

    add generated icon files
    lifted from construct UI

---
+++ types/icons/IconNames.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+export const AVATAR = 'avatar';
+export const CENTRA = 'centra';
+export const GOOGLE = 'google';
+export const LOGO = 'logo';
+export const PLUS = 'plus';
+export const SHOPIFY = 'shopify';
