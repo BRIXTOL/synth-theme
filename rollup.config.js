967b6062da9150eb674fb340325a021a3ccb3b36
Nikolas Savvidis <nicos.savidis@gmail.com>
2020-09-27 09:13:52 +0200

    various

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,37 @@
 import scss from 'rollup-plugin-scss'
-import typescript from 'rollup-plugin-typescript2'
+import ts from 'rollup-plugin-ts'
 import { terser } from 'rollup-plugin-terser'
 import { plugins } from '@brixtol/rollup-utils'
 import autoprefixer from 'autoprefixer'
 import postcss from 'postcss'
 import cssnano from 'cssnano'
 import virtual from '@rollup/plugin-virtual'
+import pkg from './package.json'
 
 export default [
+  {
+    input: 'src/index.ts',
+    external: [
+      'mithril',
+      'construct-ui',
+      'classnames'
+    ],
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true
+      },
+      {
+        file: pkg.module,
+        format: 'esm',
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      ts()
+    ]
+  },
   {
     input: 'scss',
     plugins: [
@@ -17,29 +41,13 @@ export default [
         output: 'package/synth.min.css',
         watch: [ 'style/extends', 'style/mixins' ],
         prefix: '@import "./style/extends/variables.scss";',
-        processor: css => process.env.prod ? postcss([
+        processor: css => postcss([
           autoprefixer(),
           cssnano()
-        ]) : null
+        ])
       })
     ]
-  },
-  {
-    input: 'types/synth.ts',
-    external: [ 'mithril' ],
-    output: {
-      format: 'es',
-      dir: 'package',
-      sourcemap: false
-    },
-    plugins: plugins([
-      typescript()
-    ], [
-      terser({
-        compress: {
-          passes: 2
-        }
-      })
-    ])
+
   }
+
 ]
