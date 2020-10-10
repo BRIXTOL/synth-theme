1acdc93947fb8d7b510fdd4868ac4dabd5ff9d1b
Nikolas Savvidis <nicos.savidis@gmail.com>
2020-10-10 15:34:17 +0200

    added new svgs

--- src/svg/generated/SvgNames.ts
+++ src/svg/generated/SvgNames.ts
@@ -2,10 +2,13 @@
 export const SvgNames = {
   AVATAR: 'Avatar',
   BIG_PLUS: 'BigPlus',
+  BRIXTOL: 'Brixtol',
   CENTRA: 'Centra',
+  GOOGLE_COLOUR: 'GoogleColour',
   GOOGLE: 'Google',
   LOGO: 'Logo',
-  SHOPIFY: 'Shopify'
+  SHOPIFY: 'Shopify',
+  UNIFAUN: 'Unifaun'
 }
 
 export type SvgNames = typeof SvgNames[keyof typeof SvgNames];
--- src/svg/generated/Svgs.ts
+++ src/svg/generated/Svgs.ts
@@ -4,7 +4,10 @@
 
 export { Avatar } from './custom/avatar'
 export { BigPlus } from './custom/big-plus'
+export { Brixtol } from './custom/brixtol'
 export { Centra } from './custom/centra'
+export { GoogleColour } from './custom/google-colour'
 export { Google } from './custom/google'
 export { Logo } from './custom/logo'
 export { Shopify } from './custom/shopify'
+export { Unifaun } from './custom/unifaun'
