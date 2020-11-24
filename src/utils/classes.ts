13001829e83ec23aa81bd6b0cad58f61839ecd77
Nikolas Savvidis <nicos.savidis@gmail.com>
2020-11-24 05:00:35 +0100

    class identifiers from timeline

--- src/utils/classes.ts
+++ src/utils/classes.ts
@@ -1,6 +1,15 @@
 export const Classes = {
   // Modifiers
 
-  ICON_SVG: 'synth-svg'
+  ICON_SVG: 'synth-svg',
+
+  // Timeline
+  TIMELINE: 'cui-timeline',
+  TIMELINE_ITEM: 'cui-timeline-item',
+  TIMELINE_CONTENT: 'cui-timeline-content',
+  TIMELINE_ICON: 'cui-timeline-icon',
+  TIMELINE_LEFT: 'cui-timeline-left',
+  TIMELINE_CENTER: 'cui-timeline-center',
+  TIMELINE_RIGHT: 'cui-timeline-right'
 
 };
